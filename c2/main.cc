7ff8d9376eaaeeed1dfd9f534049ed353a674438
saurabh10041998 <shindesaurabhpict@gmail.com>
2026-05-21 18:38:37 +0530

    cpp template udemy

--- c2/main.cc
+++ c2/main.cc
@@ -19,6 +19,22 @@ void B() {
 
 }
 
+// non-type template parameter
+template<int value>
+void printValue() {
+    std::cout << value << std::endl;
+}
+
+template<typename T, size_t N>
+size_t linear_search(const T(&arr)[N], const T& target) {
+    for (size_t i = 0; i < N; ++i) {
+        if (arr[i] == target) {
+            return i; // Return the index of the found element
+        }
+    }
+    return std::numeric_limits<size_t>::max(); // Return max size_t to indicate not found
+}
+
 int main() {
     foo(3u);
     foo(4);
@@ -42,4 +58,18 @@ int main() {
 
     auto res = square_constexpr(5);   // evaluated at compile time
     std::cout << res << std::endl;
+
+    // non-type template parameter call
+    printValue<10>();
+
+    int arr2[] = {10, 20, 30, 40, 50};
+    // size_t index = linear_search(arr2, 5, 30);       potentially prone to error when size is not correctly specified
+    size_t index = linear_search(arr2, 30);
+    if (index != std::numeric_limits<size_t>::max()) {
+        std::cout << "Element found at index: " << index << std::endl;
+    } else {
+        std::cout << "Element not found" << std::endl;
+    }
+
+    return 0;
 }
